b672f2880c68ec28673e5dff6430e517f6261cb4
unknown <felixaristio@gmail.com>
2023-06-15 20:44:48 +0700

    update information

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var up = document.getElementById("up");
 let result = document.getElementById("result");
 
 up.addEventListener("click",function()){
-    return result.innerHTML = "UP";
+    return result.innerHTML = "Up";
 };
