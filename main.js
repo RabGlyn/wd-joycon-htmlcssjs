2174bee31f63477c2fc03c680c44c4418d8af6b2
Felix Aristio <felixaristio@gmail.com>
2023-06-15 22:32:27 +0700

    fix UAS

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-var up = document.getElementById("up");
-let result = document.getElementById("result");
+function textup(){   
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("up");
+    document.getElementById("result").innerHTML = "UP";
+}
 
-up.addEventListener("click",function()){
-    return result.innerHTML = "Up";
-};
+function textleft(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("left");
+    document.getElementById("result").innerHTML = "LEFT";
+}
+
+function textright(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("right");
+    document.getElementById("result").innerHTML = "RIGHT";
+}
+
+function textdown(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("down");
+    document.getElementById("result").innerHTML = "DOWN";
+}
+
+function texta(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("a");
+    document.getElementById("result").innerHTML = "A";
+}
+
+function textb(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("b");
+    document.getElementById("result").innerHTML = "B";
+}
